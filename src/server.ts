376e20c670b01cc71217e0b1eba18edf0af578ad
Teodora Gjini <teodoragjini@yahoo.com>
2022-09-27 01:28:15 +0200

    //

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,49 @@
-import express from "express"
-import cors from "cors"
-import { PrismaClient } from "@prisma/client"
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import env from "dotenv";
 
-const app=express()
-app.use(cors())
-app.use(express.json())
-const prisma= new PrismaClient()
-const port=4000
+const app = express();
+app.use(cors());
+app.use(express.json());
+const prisma = new PrismaClient();
+const port = 4000;
 
+app.get(`/property`, async (req, res) => {
+  try {
+    const properties = await prisma.property.findMany({
+      include: { images: true},
+    });
+    res.send(properties);
+  } catch (error) {
+    //@ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
 
-app.get(`/property`, async (req,res)=>{
-    try{
-        const properties= await prisma.property.findMany({include:{images:true, users: true, reviews:true}})
-res.send(properties)
+app.get('/property/:id', async (req, res) => {
+    const property = await prisma.property.findUnique({
+        where:{ id: Number(req.params.id)},
+        include: {images:true, reviews:true}
+    })
+
+    if(property){
+        res.send(property)
+    } else {
+        res.status(404).send({error: 'Property not found'})
     }
-    catch(error){
-        //@ts-ignore
-res.status(400).send({error:error.message})
-    }
-
 })
 
-app.listen(port,()=>{
-    console.log(`App running at: http:/localhost:${port}`)
-})
+// app.post('/property/:id/reserve', async (req, res) => {
+//     await prisma.
+// })
+
+// app.get('/user/property' , async (req, res) => {
+//     const user = await prisma.user.
+// })
+
+app.listen(port, () => {
+  console.log(`App running at: http:/localhost:${port}`);
+});
